97dc4d653843024d116df475fdc6058d5d68c6af
Marlon Hildon <marlonhildon@gmail.com>
2021-08-18 07:09:25 -0300

    First commit - Basic structure

---
+++ src/main/java/br/com/csv/separator/CsvSeparatorBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.csv.separator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvSeparatorBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvSeparatorBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/csv/separator/CsvSeparatorBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.csv.separator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvSeparatorBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
